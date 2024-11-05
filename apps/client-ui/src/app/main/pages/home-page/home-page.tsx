e0c9aa0c60e60bb94c1d0a471405e76c9bccedb4
Pasindu Pramodya <pasindudixon25@gmail.com>
2024-11-05 19:26:21 +0530

    update profile page

--- apps/client-ui/src/app/main/pages/home-page/home-page.tsx
+++ apps/client-ui/src/app/main/pages/home-page/home-page.tsx
@@ -1,10 +1,13 @@
 import { Box } from '@mantine/core';
 import React from 'react';
+import ImageGrid from '../../components/image-grid/image-grid';
 
 const HomePage = () => {
   return <Box>
 
-    <Box>asd</Box>
+    <Box>
+      <ImageGrid/>
+    </Box>
   </Box>;
 };
 
--- apps/client-ui/src/app/main/pages/profile/pages/profile-page/profile-page.tsx
+++ apps/client-ui/src/app/main/pages/profile/pages/profile-page/profile-page.tsx
@@ -1,40 +1,65 @@
+import { useState } from 'react';
+import classes from './profile-page.module.scss';
 import {
   BackgroundImage,
-  Box, Grid,
-  Group,
-  Image, Title
+  Box, Center,
+  Flex, Group,
+  Image,
+  Paper,
+  Stack,
+  Tabs,
+  Title
 } from '@mantine/core';
 
 const ProfilePage = () => {
+  const [activeTab, setActiveTab] = useState<string | null>('first');
+
   return (
     <Box>
-      <Box mx="auto">
+      <Paper withBorder mx="auto" radius="lg" pb="md">
         <BackgroundImage
-          style={{
-            height: 300,
-          }}
+          className={classes.coverImage}
           src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-6.png"
-          radius="xs"
         >
-          <Group justify="end" align="end" w="100%" h="100%" pb="xs" pr="sm">
-            asdasdasd
+          <Group justify="center" align="end" h="100%">
+            <Image
+              className={classes.profileImage}
+              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+            />
           </Group>
         </BackgroundImage>
-      </Box>
-      <Grid>
-        <Grid.Col span={3}>
-          <Image
-            radius="md"
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
-          />
-        </Grid.Col>
-        <Grid.Col span={6}>
-        <Title order={2} c="dimmed" mt="md">
-          Brian MC
-        </Title>
-        <Title order={6}>@brianmc</Title>
-        </Grid.Col>
-      </Grid>
+        <Box mt="xl">
+          <Flex mt="xl" justify="center" direction="column" align="center">
+            <Stack gap={0} mt="sm">
+              <Center>
+                <Title order={3} mt="md">
+                  Brian MC
+                </Title>
+              </Center>
+              <Center>
+                <Title order={6} c="dimmed">
+                  @brianmc
+                </Title>
+              </Center>
+            </Stack>
+          </Flex>
+        </Box>
+        <Box px="md">
+          
+              <Tabs value={activeTab} onChange={setActiveTab}>
+                <Tabs.List>
+                  <Tabs.Tab value="first">First tab</Tabs.Tab>
+                  <Tabs.Tab value="second">Second tab</Tabs.Tab>
+                </Tabs.List>
+
+                <Tabs.Panel value="first">
+                  <Box>s</Box>
+                </Tabs.Panel>
+                <Tabs.Panel value="second">Second panel</Tabs.Panel>
+              </Tabs>
+           
+        </Box>
+      </Paper>
     </Box>
   );
 };
